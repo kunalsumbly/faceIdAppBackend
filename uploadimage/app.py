37faebc8c57c0c9ce96f93973fa8328d49de9292
kunalsumbly <kunal.sumbly@gmail.com>
2020-10-11 17:50:06 +1100

    modified the upload image lambda logic

--- uploadimage/app.py
+++ uploadimage/app.py
@@ -5,6 +5,7 @@ import json
 
 BUCKET = os.environ['BUCKETNAME']
 COLLECTION_ID = os.environ['REKOGNITIONCOLLECTION']
+FACE_MATCH_THRESHOLD = int(os.environ['REKOGNITIONFACEMATCHTHRESHOLD'])
 LOG_LEVEL = logging.DEBUG
 
 s3 = boto3.client('s3')
@@ -24,37 +25,46 @@ def lambda_handler(event, context):
         image = data['image']
         missing_person_data = data['missingpersondata']
         logger.info("Indexing faces.")
-        response = rekognition.index_faces(
-            Image={
-                "S3Object": {
-                    "Bucket": BUCKET,
-                    "Name": image
-                }
-            },
-            CollectionId=COLLECTION_ID
+        
+        # before we add the image to index check if the face is already registered in rekognition
+        response = rekognition.search_faces_by_image(
+                    Image={
+                        "S3Object": {
+                            "Bucket": BUCKET,
+                            "Name": image
+                        }
+                    },
+                    CollectionId=COLLECTION_ID,
+                    FaceMatchThreshold=FACE_MATCH_THRESHOLD,
+                    MaxFaces=1
         )
-    
-        logger.info("Getting results.")
 
-        if response['ResponseMetadata']['HTTPStatusCode'] != 200 or len(response['FaceRecords']) == 0:
-            raise Exception("Fail to register a face to Rekognition.")
-
-        faceId = response['FaceRecords'][0]['Face']['FaceId']
-        logger.info("Recorded faceId: {}".format(faceId))
-
-        # save the attributes in the person missing data
-        saveMissingPersonData(missing_person_data,faceId)
-
-        return {
-            "statusCode": 200,
-            "headers": {
-                "Access-Control-Allow-Origin": "*",
-                "Content-Type": "application/json"
-            },
-            "body": json.dumps({
-                "message": "Success. Face recorded"
-            }),
-        }
+        if len(response['FaceMatches']) == 0:
+            # if the face does not exist already then we add that face to the index collection
+            response = registerFaceInRekognitionCollection(image, response, missing_person_data)
+            return {
+                "statusCode": 200,
+                "headers": {
+                    "Access-Control-Allow-Origin": "*",
+                    "Content-Type": "application/json"
+                },
+                "body": json.dumps({
+                    "message": "Success. Face recorded"
+                }),
+            }
+        else: # face already is registered so get the already registered details
+            # in either case we will get a face id 
+            message = searchExistingDataByFaceId(response)
+            return {
+                "statusCode": 200,
+                "headers": {
+                    "Access-Control-Allow-Origin": "*",
+                    "Content-Type": "application/json"
+                },
+                "body": json.dumps({
+                    "message": message
+                }),
+            }
     except Exception as e:
         logger.error(e)
         return {
@@ -67,6 +77,39 @@ def lambda_handler(event, context):
                 "message": "Error: {}".format(e),
             }),
         }
+
+def searchExistingDataByFaceId(response):
+    faceId = response['FaceMatches'][0]['Face']['FaceId']
+    logger.info("Recorded faceId: {}".format(faceId))
+    faceIdResponse = findPersonDataByFaceId(faceId)
+    # save the attributes in the person missing data
+    payload=faceIdResponse['Item']
+    firstname = [*payload['firstname'].values()]
+    lastname=[*payload['lastname'].values()]
+    message="Face already registered under faceid="+faceId+", firstname="+firstname[0]+",lastname="+lastname[0]
+    return message
+
+def registerFaceInRekognitionCollection(image, response, missing_person_data):
+    # if the face does not exist already then we add that face to the index collection
+    response = rekognition.index_faces(
+                Image={
+                    "S3Object": {
+                        "Bucket": BUCKET,
+                        "Name": image
+                    }
+                },
+                CollectionId=COLLECTION_ID
+            )
+    logger.info("Getting results.")
+
+    if response['ResponseMetadata']['HTTPStatusCode'] != 200 or len(response['FaceRecords']) == 0:
+        raise Exception("Fail to register a face to Rekognition.")
+    # new person data create record flow
+    faceId = response['FaceRecords'][0]['Face']['FaceId']
+    logger.info("Recorded faceId: {}".format(faceId))
+    # save the attributes in the person missing data
+    saveMissingPersonData(missing_person_data,faceId)
+    return response
 
 # This method saves the missing person data in Dynamodb
 def saveMissingPersonData(missing_person_data, faceId):
@@ -84,4 +127,11 @@ def saveMissingPersonData(missing_person_data, faceId):
                                 "dateofreport":{"S":missing_person_data['dateofreport']}
                             }
                 )
-
+def findPersonDataByFaceId(faceId):
+    response = dynamodb.get_item (
+                TableName=missing_person_table,
+                    Key={
+                        "faceid" :{"S":faceId}
+                    }
+            )
+    return response
