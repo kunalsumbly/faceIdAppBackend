e5e45bda41b22d01fd1155e182aaf01118e86212
kunalsumbly <kunal.sumbly@gmail.com>
2020-10-17 22:44:57 +1100

    working kinesis stream soltuion in us-west-2

--- uploadimage/app.py
+++ uploadimage/app.py
@@ -39,9 +39,13 @@ def lambda_handler(event, context):
                     MaxFaces=1
         )
 
+        logger.info("After Searching in rekognition")
+
         if len(response['FaceMatches']) == 0:
+            logger.info("Face Not Found, so it is a register case")
             # if the face does not exist already then we add that face to the index collection
             response = registerFaceInRekognitionCollection(image, response, missing_person_data)
+            logger.info("return register response")
             return {
                 "statusCode": 200,
                 "headers": {
@@ -54,6 +58,7 @@ def lambda_handler(event, context):
             }
         else: # face already is registered so get the already registered details
             # in either case we will get a face id 
+            logger.info("Face already exists")
             message = searchExistingDataByFaceId(response)
             return {
                 "statusCode": 200,
@@ -79,14 +84,17 @@ def lambda_handler(event, context):
         }
 
 def searchExistingDataByFaceId(response):
+    logger.info("inside searchExistingDataByFaceId")
     faceId = response['FaceMatches'][0]['Face']['FaceId']
     logger.info("Recorded faceId: {}".format(faceId))
     faceIdResponse = findPersonDataByFaceId(faceId)
     # save the attributes in the person missing data
-    payload=faceIdResponse['Item']
-    firstname = [*payload['firstname'].values()]
-    lastname=[*payload['lastname'].values()]
-    message="Face already registered under faceid="+faceId+", firstname="+firstname[0]+",lastname="+lastname[0]
+    if(faceIdResponse['Item']):
+        logger.info("fetch db item record")
+        payload=faceIdResponse['Item']
+        firstname = [*payload['firstname'].values()]
+        lastname=[*payload['lastname'].values()]
+        message="Face already registered under faceid="+faceId+", firstname="+firstname[0]+",lastname="+lastname[0]
     return message
 
 def registerFaceInRekognitionCollection(image, response, missing_person_data):
@@ -100,7 +108,7 @@ def registerFaceInRekognitionCollection(image, response, missing_person_data):
                 },
                 CollectionId=COLLECTION_ID
             )
-    logger.info("Getting results.")
+    logger.info("After index in the Rekognition")
 
     if response['ResponseMetadata']['HTTPStatusCode'] != 200 or len(response['FaceRecords']) == 0:
         raise Exception("Fail to register a face to Rekognition.")
@@ -108,11 +116,14 @@ def registerFaceInRekognitionCollection(image, response, missing_person_data):
     faceId = response['FaceRecords'][0]['Face']['FaceId']
     logger.info("Recorded faceId: {}".format(faceId))
     # save the attributes in the person missing data
+    logger.info("Saving in the Dynamodb starts")
     saveMissingPersonData(missing_person_data,faceId)
+    logger.info("Saving in the Dynamodb done")
     return response
 
 # This method saves the missing person data in Dynamodb
 def saveMissingPersonData(missing_person_data, faceId):
+    logger.info("Inside saveMissingPersonData")
     response = dynamodb.put_item (
                     TableName=missing_person_table,
                      Item = {
@@ -128,10 +139,12 @@ def saveMissingPersonData(missing_person_data, faceId):
                             }
                 )
 def findPersonDataByFaceId(faceId):
+    logger.info("findPersonDataByFaceId starts")
     response = dynamodb.get_item (
                 TableName=missing_person_table,
                     Key={
                         "faceid" :{"S":faceId}
                     }
             )
+    logger.info("findPersonDataByFaceId ends")
     return response
